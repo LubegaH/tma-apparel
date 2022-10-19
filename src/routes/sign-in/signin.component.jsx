78c823a58848e366f1c5f68c8aba789f5f99c438
LubegaH <lubegah@gmail.com>
2022-10-19 22:22:51 +0300

    Tried different sign in method

--- src/routes/sign-in/signin.component.jsx
+++ src/routes/sign-in/signin.component.jsx
@@ -5,14 +5,29 @@ import {
 } from '../../utils/firebase/firebase.utils';
 
 const Signin = () => {
+  //   useEffect(() => {
+  //     async function fetchData() {
+  //       const response = await getRedirectResult(auth);
+
+  //       if (response) {
+  //         const userDocRef = await createUserDocumentFromAuth(response.user);
+  //       }
+  //     }
+  //     fetchData();
+  //   }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h2>Sign in page</h2>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google redirect
+      </button> */}
     </div>
   );
 };
